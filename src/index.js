8a0fed20463bdf8a10fcb5af9d1a386d003cb36d
mamico <mauro.amico@gmail.com>
2022-05-19 13:49:34 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import helmet from 'helmet';
+
 const applyConfig = (config) => {
+  if (__SERVER__) {
+    const middleware = helmet();
+    middleware.id = 'helmet-middleware';
+
+    config.settings.expressMiddleware = [
+      ...config.settings.expressMiddleware,
+      middleware,
+    ];
+  }
   return config;
 };
 
