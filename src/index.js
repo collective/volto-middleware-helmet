86338ef4ce46af1ef31484e9bafdf2bef507ac73
mamico <mauro.amico@gmail.com>
2022-05-20 13:39:39 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -2,10 +2,38 @@ import helmet from 'helmet';
 
 // TODO: relaxed profile for dev ?
 const profiles = {
-  strict: {},
+  default: {
+    contentSecurityPolicy: {
+      directives: {
+        // TODO: need improvements
+        scriptSrc: ["'self'", 'https:', "'unsafe-inline'"],
+      },
+    },
+  },
+  development: {
+    hsts: false,
+    contentSecurityPolicy: {
+      scriptSrc: ["'self'", 'https:', "'unsafe-inline'"],
+      reportOnly: true,
+    },
+    crossOriginResourcePolicy: false,
+    crossOriginOpenerPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  },
 };
 
 const applyConfig = (config) => {
+  const defaults = {
+    helmetSettings: process.env.HELMET_PROFILE
+      ? process.env.HELMET_PROFILE
+      : __DEVELOPMENT__
+      ? 'development'
+      : 'default',
+  };
+  config.settings = {
+    ...config.settings,
+    ...defaults,
+  };
   if (__SERVER__) {
     const settings =
       profiles[config.settings?.helmetSettings] === undefined
